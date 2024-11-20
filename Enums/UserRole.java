cbc66e0a587697b82974647c0bb11ce0d97ca914
jordi2987 <90922512+jordi2987@users.noreply.github.com>
2024-11-20 19:56:17 +0800

    Update UserRole.java

--- Enums/UserRole.java
+++ Enums/UserRole.java
@@ -1,5 +1,9 @@
 package Enums;
 
+/**
+ * Enum representing the different user roles in the hospital management system.
+ * Each role is associated with a string value that is used to represent it in the system.
+ */
 public enum UserRole {
     DOCTOR("doctor"),
     ADMINISTRATOR("administrator"),
@@ -8,11 +12,22 @@ public enum UserRole {
 
     private final String value;
 
-    // Constructor to assign the lowercase value
+
+    /**
+     * Constructor for the enum, assigning a string value to each role.
+     *
+     * @param value the string value representing the user role (e.g., "doctor", "administrator")
+     */
     UserRole(String value) {
         this.value = value;
     }
 
+    /**
+     * Returns the string representation of the user role.
+     * The string is returned in lowercase format.
+     *
+     * @return the lowercase value of the user role (e.g., "doctor")
+     */
     // Override toString to return the lowercase value
     @Override
     public String toString() {
