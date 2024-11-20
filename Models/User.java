70da3176de904b7429b21667fa42421f6aa7f5ab
jordi2987 <90922512+jordi2987@users.noreply.github.com>
2024-11-20 19:50:35 +0800

    Update User.java

--- Models/User.java
+++ Models/User.java
@@ -4,6 +4,8 @@ import Enums.UserRole;
 
 /**
  * Represents a user in the hospital management system.
+ * This class holds user details such as ID, role, name, gender, and age.
+ * It provides methods to access and modify certain user information.
  */
 public class User {
     private final String userId; // User ID is final, as it should not change
@@ -13,6 +15,16 @@ public class User {
     private String gender;
     private int age;
 
+    /**
+     * Constructs a new user object with the specified details.
+     *
+     * @param userId    The unique ID for each user.
+     * @param password  The user's password.
+     * @param role      The role of the user in the HMS (Patient, Doctor, Pharmacist, Administrator)
+     * @param name      The user's name.
+     * @param gender    The user's gender.
+     * @param age       The user's age.
+     */
     public User(String userId, String password, UserRole role, String name, String gender, int age) {
         this.userId = userId;
         this.password = password;
@@ -22,38 +34,83 @@ public class User {
         this.age = age;
     }
 
+    /**
+     * Gets the Unique ID of the user.
+     *
+     * @return The user's ID
+     */
     public String getUserId() {
         return userId;
     }
 
+    /**
+     * Gets the role of the user.
+     *
+     * @return The user's role.
+     */
     public UserRole getRole() {
         return role;
     }
 
+    /**
+     * Gets the password of the user.
+     *
+     * @return The user's password.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Gets the name of the user.
+     *
+     * @return The user's name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the role of the user.
+     *
+     * @param name The user's new name.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the gender of the user.
+     *
+     * @return The user's gender.
+     */
     public String getGender() {
         return gender;
     }
 
+    /**
+     * Gets the role of the user.
+     *
+     * @param gender The user's gender.
+     */
     public void setGender(String gender) {
         this.gender = gender;
     }
 
+    /**
+     * Gets the age of the user.
+     *
+     * @return The user's age.
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * Sets the age of the user.
+     *
+     * @param age The user's age.
+     */
     public void setAge(int age) {
         this.age = age;
     }
