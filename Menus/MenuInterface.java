d364d6647344c6db9a45b7efff7012ec8e729cfe
jordi2987 <90922512+jordi2987@users.noreply.github.com>
2024-11-20 19:54:43 +0800

    Update MenuInterface.java

--- Menus/MenuInterface.java
+++ Menus/MenuInterface.java
@@ -1,5 +1,10 @@
 package Menus;
 
 public interface MenuInterface {
+    /**
+     * Displays the available menu options and returns the user's choice.
+     *
+     * @return the selected option as an integer.
+     */
     int displayOptions();
 }
